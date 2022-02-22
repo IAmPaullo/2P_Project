789dce4f2c84f384e3fa429271757703691a7ade
Paullo B Felipe <iampaullo@live.com>
2022-02-22 16:43:11 -0300

    state machine

--- Assets/Scripts/EnemyVision.cs
+++ Assets/Scripts/EnemyVision.cs
@@ -4,24 +4,36 @@ using UnityEngine;
 
 public class EnemyVision : MonoBehaviour
 {
-
+    [SerializeField] int maxBounce = 1;
+    [SerializeField] int states = 3;
+    [SerializeField] float cooldown;
+    [SerializeField] GameObject rocketPrefab;
+    Transform originalTransform;
     Ray2D ray;
     RaycastHit2D hit;
-
     Vector2 refDir;
-    LineRenderer lineRenderer;
-    public int maxBounce = 2;
+
+
+
+    int randValue;
+    bool canRoll = true;
+    bool isSpawnable = true;
+
+    [SerializeField]
+    enum EnemyStanceEnum { Idle, Reset, ShootWall, ShootStraight };
+
+    [SerializeField]
+    EnemyStanceEnum stance;
+
 
     private void Awake()
     {
-        //lineRenderer = GetComponent<LineRenderer>();
-        
+        originalTransform = this.gameObject.transform;
     }
 
     private void Update()
     {
         Signal();
-
 
     }
 
@@ -33,34 +45,44 @@ public class EnemyVision : MonoBehaviour
         hit = Physics2D.Raycast(ray.origin, ray.direction);
         Debug.DrawRay(transform.position, this.gameObject.transform.up, Color.red);
 
-        
-
-        for (int i = 0; i < maxBounce; i++)
+        if (canRoll)
         {
-            if (i == 0)
+            int roll = RollForChance();
+            EnemyState(roll);
+
+            StartCoroutine(ChanceCountDown());
+            canRoll = false;
+        }
+
+        if (randValue != 0)
+        {
+
+
+            if (hit && hit.collider.CompareTag("Wall"))
             {
-                if (hit)
+                refDir = Vector2.Reflect(ray.direction, hit.normal);
+                RaycastHit2D reflectedHit = Physics2D.Raycast(hit.point, refDir);
+
+
+                Debug.Log("parede");
+
+                Debug.DrawRay(hit.point, refDir * 100, Color.green);
+
+                if (reflectedHit)
                 {
-                    refDir = Vector2.Reflect(ray.direction, hit.normal);
-                    //ray = new Ray2D(hit.point, refDir);
-
-                    RaycastHit2D reflectedHit = Physics2D.Raycast(hit.point, refDir);
-
-                    
-                    
-                    
-                    Debug.DrawRay(hit.point, refDir * 100, Color.green);
-                    if (reflectedHit)
+                    if (reflectedHit.collider.CompareTag("Ball"))
                     {
-                        if (reflectedHit.collider.tag == "Ball")
-                        {
-                            //Debug.Log("bola");
-                        }
-
-                        //Debug.Log(hit.transform.name);
+                        Debug.Log("Collider verde na bola");
                     }
+                    else
+                    {
+
+                        Debug.Log(hit.transform.name);
+                    }
+
                 }
             }
+
             else
             {
                 if (hit)
@@ -68,13 +90,89 @@ public class EnemyVision : MonoBehaviour
                     refDir = Vector2.Reflect(refDir, hit.normal);
                     ray = new Ray2D(hit.point, refDir);
                     Debug.DrawRay(hit.point, refDir * 100, Color.yellow);
-
-                    
-
-                    //Debug.Log(hit.transform.name);
-
                 }
             }
         }
+
     }
+
+
+    private IEnumerator ChanceCountDown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        canRoll = true;
+    }
+
+    private int RollForChance()
+    {
+        if (canRoll)
+        {
+            randValue = Random.Range(0, states);
+            Debug.Log(randValue);
+        }
+
+        return randValue;
+
+    }
+
+
+    private void SpawnMissile()
+    {
+
+        rocketPrefab.SetActive(true);
+        canRoll = false;
+        StartCoroutine(ChanceCountDown());
+    }
+
+    private void ResetTransform()
+    {
+        transform.position = originalTransform.position;
+        transform.rotation = originalTransform.rotation;
+        ray = new Ray2D(this.transform.position, transform.up);
+        hit = Physics2D.Raycast(ray.origin, ray.direction);
+    }
+
+    private void EnemyState(int rolledState)
+    {
+
+
+        switch (rolledState)
+        {
+            case 0:
+                //Idle
+                stance = EnemyStanceEnum.Idle;
+                ray = new Ray2D(this.transform.position, transform.up);
+                hit = Physics2D.Raycast(ray.origin, ray.direction);
+                break;
+
+
+            case 1:
+                stance = EnemyStanceEnum.Reset;
+                ResetTransform();
+
+                break;
+
+               
+
+            case 2:
+                stance = EnemyStanceEnum.ShootStraight;
+
+                break;
+
+
+            case 3:
+                stance = EnemyStanceEnum.ShootWall;
+
+                break;
+
+
+
+        }
+
+
+
+    }
+
+
+
 }
